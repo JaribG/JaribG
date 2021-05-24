f18135c66be6249abd0f6eedfb10b68b45a22a8f
Jarib Gomera <jaribg24@gmail.com>
2021-05-24 12:39:10 -0400

    subir

--- OneDrive/Escritorio/Trabajo/3simpleERPAutomation/Login/1validaciondellogincorrecto.js
+++ OneDrive/Escritorio/Trabajo/3simpleERPAutomation/Login/1validaciondellogincorrecto.js
@@ -7,7 +7,7 @@ const options = new firefox.Options();
 options.setProfile("./Seleniumprueba");
 
 
-describe('Filtrodebusqueda', function() {
+describe('Simple erp', function() {
   this.timeout(500000);
   let driver
   let vars
@@ -18,7 +18,7 @@ describe('Filtrodebusqueda', function() {
  // afterEach(async function() {
  //  await driver.quit();
  // })  
-  it('Filtrodebusqueda', async function() {
+  it('Simple erp', async function() {
     await driver.get("https://simple-erp-299814.web.app/ventas");
     await driver.manage().window().maximize();
     await driver.manage().setTimeouts( { implicit: 60000 } );
@@ -26,7 +26,9 @@ describe('Filtrodebusqueda', function() {
     await driver.findElement(By.css(".firebaseui-idp-icon")).click();
     await driver.wait(until.elementsLocated(By.id("identifierId")), 50000);
     await driver.findElement(By.id("identifierId")).sendKeys("jaribg24@gmail.com");
-    await driver.findElement(By.css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > .VfPpkd-RLmnJb")).click()
+    await driver.findElement(By.css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > .VfPpkd-RLmnJb")).click();
+    await driver.wait(until.elementsLocated(By.css(".VmOpGe")), 50000);
+    await driver.findElement(By.css(".VmOpGe")).click();
     await driver.wait(until.elementsLocated(By.name("password")), 50000);
     await driver.findElement(By.name("password")).click()
     await driver.findElement(By.name("password")).sendKeys("Jarib0121")
